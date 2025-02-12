2a88428baabc5f5c548b0bedb27e3e4a6a412c10
“Essam” <essamali7698@gmail.com>
2025-02-13 00:17:36 +0200

    adding depoly pages scrips

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/reactjs_project_eCommerce/',
   plugins: [react(), tailwindcss()],
 })
